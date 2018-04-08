116ddb077cdaabbe648de01da2aa8836c7c7eb10
DajhMaster <dman977@gmail.com>
2018-04-07 19:27:46 -0600

    Forgot the content...

---
+++ modules/module.dajh/commands.js
@@ -0,0 +1,14 @@
+module.exports = { 
+    doPing: async ( obj ) => {
+      return await require('./ping.js').doPing( obj );
+    }, 
+    doParse: async ( obj ) => {
+      return await require('./parse.js').doParse( obj );
+    }, 
+    doUrban: async ( obj ) => {
+      return await require('./urban.js').doUrban( obj );
+    }, 
+    urbanRandom: async ( obj ) => {
+      return await require('./urban.js').urbanRandom( obj );
+    }
+  };
